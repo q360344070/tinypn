ef86205ce9ef3f99711b3ab1ba3e1e5b4e3887aa
q360344070 <xiao360344070@126.com>
2018-09-12 13:45:06 +0800

    Signed-off-by: q360344070 <xiao360344070@126.com>

--- tinypng/Program.cs
+++ tinypng/Program.cs
@@ -1,15 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace tinypng
 {
     class Program
     {
+        private static string path;
         static void Main(string[] args)
         {
+            path = "E:/ShanHe1.0.1/Assets/Models";
+            compression pngco = new compression();
+            pngco.CheckKey().Wait();
+
+            int k = pngco.GeNumberOfCompressions();
+            List< FileInfo > keList= FileGet.getFile(path, ".png.jpg");
+            int ke = 0;
+            int index = 0;
+            for (int i = 0; i < keList.Count; i++)
+            {
+                Task tes=Task.Run(() =>
+                {
+                    FileInfo tanseInfo = keList[index];
+                    compression.compressionPng(tanseInfo.FullName, delegate ()
+                    {
+                        ke++;
+                        Console.WriteLine("已经压缩完成" + ke);
+                    });
+                    index++;
+                    Console.WriteLine("启动Task=" + tanseInfo.FullName);
+                });
+            }
+            while (ke < keList.Count - 1)
+            {
+                Console.ReadKey();
+            }
         }
     }
 }
---
+++ tinypng/compression.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TinifyAPI;
+
+namespace tinypng
+{
+    class compression
+    {
+        private string key;
+
+        public string Key
+        {
+            get
+            {
+                return key;
+            }
+
+            set
+            {
+                key = value;
+            }
+        }
+
+        public async Task CheckKey()
+        {
+            try
+            {
+                Tinify.Key = "Wud9BHyk6PGEALoQJ94O00HHrkfFkAky";
+                await Tinify.Validate();
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+                // Validation of API key failed.
+            }
+        }
+        public int GeNumberOfCompressions()
+        {
+            var compressionsThisMonth = Tinify.CompressionCount;
+            if (compressionsThisMonth == null)
+            {
+                return 0;
+            }
+            return (int) compressionsThisMonth.Value;
+        }
+        public static async Task compressionPng(string path,Action claa)
+        {
+            try
+            {
+                var source = Tinify.FromFile(path);
+                await source.ToFile(path);
+                if (claa != null)
+                {
+                    claa.Invoke();
+                }
+            }
+            catch (AccountException e)
+            {
+                System.Console.WriteLine("The error message is:key压缩数量已达到上限 " + e.Message);
+                // Verify your API key and account limit.
+            }
+            catch (ClientException e)
+            {
+                // Check your source image and request options.
+                Console.WriteLine("由于提交的数据存在问题，无法完成请求。异常消息将包含更多信息。您不应该重试该请求。");
+            }
+            catch (ServerException e)
+            {
+                // Temporary issue with the Tinify API.
+                Console.WriteLine("由于Tinify API存在临时问题，无法完成请求。几分钟后重试请求是安全的。如果您在较长时间内反复看到此错误，请 与我们联系。");
+            }
+            catch (ConnectionException e)
+            {
+                Console.WriteLine("无法发送请求，因为连接到Tinify API时出现问题。您应该验证您的网络连接。重试请求是安全的。");
+                // A network connection error occurred.
+            }
+            catch (System.Exception e)
+            {
+                // Something else went wrong, unrelated to the Tinify API.
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
