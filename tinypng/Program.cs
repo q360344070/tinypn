f910c37164887b3811eab4d4e8a0b15515612a63
q360344070 <xiao360344070@126.com>
2019-05-09 16:18:05 +0800

    Update Program.cs

--- tinypng/Program.cs
+++ tinypng/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,8 +13,8 @@ namespace tinypng
         private static string path;
         static void Main(string[] args)
         {
-            //path = "E:/ShanHe1.0.1/Assets";
-            path = "E:/ShanHe1.0.1/Assets/Textures/UI/BuildName/tips.png";
+            path = "C:\\Users\\Administrator\\Desktop\\友宝小游戏文档\\友宝便利店\\切图";
+            //path = "E:/ShanHe1.0.1/Assets/Textures/UI/BuildName/tips.png";
             //path = "E:/ShanHe1.0.1/Assets/Textures/UI/Dynamic/Common";
 
             compression pngco = new compression();
@@ -22,34 +22,64 @@ namespace tinypng
             kje.GetAwaiter();
             int k = pngco.GeNumberOfCompressions();
             List< FileInfo > keList= FileGet.getFile(path, ".png|.jpg");
+            List<Task> tasklist = new List<Task>();
             int ke = 0;
-            int index = -1;
-            for (int i = 0; i < keList.Count; i++)
-            {
-                //Thread threadB = new Thread(
-                //() =>
-                //{
-                //    index++;
-                //    if (index < keList.Count)
-                //    {
-                //        compression.compressionPng(keList[index].FullName, delegate ()
-                //        {
-                //            ke++;
-                //            Console.WriteLine("已经压缩完成" + ke);
-                //        }).Wait();
-                //    }
-                //}
-                //);
-                //threadB.Start();
-                compression.compressionPng(keList[i].FullName, delegate ()
-                {
-                    ke++;
-                    Console.WriteLine("已经压缩完成" + ke);
-                }).Wait();
-            }
+            bool isCamp = false;
+            bool flag = true;
             while (ke < keList.Count - 1)
             {
-                Console.ReadKey();
+                flag = !(ke < keList.Count - 1);
+                //Sleep
+                if (!isCamp)
+                {
+                    isCamp = true;
+
+                    int index = -1;
+                    for (int i = 0; i < keList.Count; i++)
+                    {
+                        //Thread threadB = new Thread(
+                        //() =>
+                        //{
+                        //    index++;
+                        //    if (index < keList.Count)
+                        //    {
+                        //        compression.compressionPng(keList[index].FullName, delegate ()
+                        //        {
+                        //            ke++;
+                        //            Console.WriteLine("已经压缩完成" + ke);
+                        //        }).Wait();
+                        //    }
+                        //}
+                        //);
+                        //threadB.Start();
+                        string namefile = keList[i].FullName;
+                        Task t2 = new Task(() => {
+                            compression.compressionPng(namefile, delegate ()
+                            {
+                                ke++;
+                                Console.WriteLine("已经压缩完成" + ke);
+                            }).Wait();
+                        });
+                        tasklist.Add(t2);
+                    }
+                    int counout = 0;
+                    List<Task> tasklist1 = new List<Task>();
+                    for (int j = tasklist.Count-1; j >-1; j--)
+                    {
+                        tasklist[j].Start();
+                        tasklist1.Add(tasklist[j]);
+                        tasklist.RemoveAt(j);
+                        counout++;
+                        if (counout > 18)
+                        {
+                            Task.WaitAll(tasklist1.ToArray());
+                            tasklist1.Clear();
+                            counout = 0;
+                        }
+                    }
+                    Task.WaitAll(tasklist1.ToArray());
+                }
+                Thread.Sleep(1);
             }
         }
     }
